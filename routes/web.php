1e35208e56a2dc2a0d34b69977aa96cf88cb5ab9
Victor Ferreira <vitfera@hotmail.com>
2025-06-22 18:03:18 -0300

    comentando rotas

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,28 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Painel\DepartamentoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/teste', function () {
-    return view('painel.dashboard.index');
-});
 
-Route::get('/dashboard', function () {
-    return view('painel.dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//('/teste', function () {
+    //return view('painel.dashboard.index');
+//});
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//Route::get('/dashboard', function () {
+    //return view('painel.dashboard.index');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+//Route::middleware('auth')->group(function () {
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
-Auth::routes();
+//require __DIR__.'/auth.php';
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
