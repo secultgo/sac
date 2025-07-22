d7304521aba1d3b8746520259da57247612a7ebe
Victor Ferreira <vitfera@hotmail.com>
2025-07-21 22:56:45 -0300

    correção arquivos que não precisam de alteração

--- routes/web.php
+++ routes/web.php
@@ -13,44 +13,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('painel')->group(function () {
-    Route::resource('departamentos', DepartamentoController::class);
-    Route::resource('problemas', ProblemaController::class);
-    Route::put('problemas/{problema}/desativar', [ProblemaController::class, 'desativar'])->name('problemas.desativar');
-    Route::put('problemas/{problema}/ativar', [ProblemaController::class, 'ativar'])->name('problemas.ativar');
+Route::prefix('painel')
+     ->group(function () {
+         Route::resource('departamentos', DepartamentoController::class);
 
-    Route::resource('servicos', ServicoChamadoController::class)->parameters(['servicos' => 'servicosChamado']);
-    Route::resource('locais', LocalController::class)->parameters(['locais' => 'local']);
+         Route::resource('problemas', ProblemaController::class);
+         Route::put('problemas/{problema}/desativar', [ProblemaController::class, 'desativar'])
+            ->name('problemas.desativar');
+         Route::put('problemas/{problema}/ativar', [ProblemaController::class, 'ativar'])
+            ->name('problemas.ativar');
 
-    Route::get('/usuarios/{usuario}/edit-nivel', [UserController::class, 'edit_nivel'])->name('usuarios.edit_nivel');
-    Route::put('/usuarios/{usuario}/nivel', [UserController::class, 'updateNivel'])->name('usuarios.update_nivel');
+        Route::resource('servicos', ServicoChamadoController::class)->parameters(['servicos' => 'servicosChamado']);
 
-    Route::resource('usuarios', UserController::class)->names('usuarios')->except(['show']);
-    Route::get('/painel/usuarios', [UserController::class, 'index'])->name('painel.usuarios.index');
+        Route::resource('locais', LocalController::class)->parameters(['locais' => 'local']);
 
-    Route::put('/usuarios/{usuario}/ativar', [UserController::class, 'ativar'])->name('usuarios.ativar');
-    Route::put('/usuarios/{usuario}/desativar', [UserController::class, 'desativar'])->name('usuarios.desativar');
-    Route::get('/usuarios/importar-ldap', [UserController::class, 'importFromLdap'])->name('usuarios.importar.ldap');
+        Route::resource('usuarios', UserController::class)->names('usuarios')->except(['show']);
+        Route::get('usuarios', [UserController::class, 'index'])->name('painel.usuarios.index');
+        Route::get('usuarios/{usuario}/edit-nivel', [UserController::class, 'edit_nivel'])->name('usuarios.edit_nivel');
+        Route::put('usuarios/{usuario}/nivel', [UserController::class, 'updateNivel'])->name('usuarios.update_nivel');
+        Route::put('usuarios/{usuario}/ativar', [UserController::class, 'ativar'])->name('usuarios.ativar');
+        Route::put('usuarios/{usuario}/desativar', [UserController::class, 'desativar'])->name('usuarios.desativar');
+        Route::get('usuarios/importar-ldap', [UserController::class, 'importFromLdap'])->name('usuarios.importar.ldap');
 
-    Route::resource('ldap', LdapController::class);
-});
+        Route::resource('ldap', LdapController::class);
+     });
 
-// ('/teste', function () {
-//     return view('painel.dashboard.index');
-// });
+//('/teste', function () {
+    //return view('painel.dashboard.index');
+//});
 
-// Route::get('/dashboard', function () {
-//     return view('painel.dashboard.index');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/dashboard', function () {
+    //return view('painel.dashboard.index');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+//Route::middleware('auth')->group(function () {
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
-// require __DIR__.'/auth.php';
+//require __DIR__.'/auth.php';
 
-// Auth::routes();
+//Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
