95a82973bcbdf6a166dce95ef5beff8dbeeb8652
Victor Ferreira <vitfera@hotmail.com>
2025-09-18 23:35:21 -0300

    fix: implementar favicon com brasão do sistema

--- config/adminlte.php
+++ config/adminlte.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'SAC | Login') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
